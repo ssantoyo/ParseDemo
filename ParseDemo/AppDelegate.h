ea9c0bb0df7251e1ffa919d9c7aebf39f7f1b675
Sergio Santoyo <ssantoyo@fb.com>
2020-07-06 08:21:42 -0700

    Initial Commit

---
+++ ParseDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseDemo
+//
+//  Created by Sergio Santoyo on 7/6/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ParseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseDemo
+//
+//  Created by Sergio Santoyo on 7/6/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
